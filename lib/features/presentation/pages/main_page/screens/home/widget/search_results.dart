70cd80c30ee68b98d39444c460d4b1ce36178caf
Favour <idowufavour07@gmail.com>
2025-10-05 17:48:52 +0100

    implemented animated image card

---
+++ lib/features/presentation/pages/main_page/screens/home/widget/search_results.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../../../../core/utils/formatters.dart';
+import '../../../../../providers/saved_episodes/saved_episode_provider.dart';
+import 'custom_tile.dart';
+
+class SearchResults extends ConsumerWidget {
+  const SearchResults({super.key, required this.episodes});
+
+  final List episodes;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ListView.separated(
+      padding: EdgeInsets.symmetric(horizontal: 12.w),
+      itemCount: episodes.length,
+      separatorBuilder: (_, __) => 12.verticalSpace,
+      itemBuilder: (context, index) {
+        final episode = episodes[index];
+        return CustomTile(
+          title: episode.title,
+          subtitle: formatAudioLength(episode.audioLength),
+          imageUrl: episode.image,
+          onPressed: () {
+            // TODO: Navigate to details
+          },
+          onPlayIconPressed: () {
+            // TODO: Play episode
+          },
+          onSaveIconPressed: () {
+            ref
+                .read(savedEpisodeProvider.notifier)
+                .toggleSavedEpisode(episode: episode);
+          },
+          isSaved: ref.watch(savedEpisodeProvider).maybeWhen(
+                data: (savedEpisodes) =>
+                    savedEpisodes.any((e) => e.id == episode.id),
+                orElse: () => false,
+              ),
+        );
+      },
+    );
+  }
+}
